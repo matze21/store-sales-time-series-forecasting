96cddc477e82171424c16926f5b7f8279ee26e36
Matthias Kargl <matthias-kargl@t-online.de>
2024-03-04 17:49:23 -0800

    add amira stuff

---
+++ analysis/baseFunctions.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime
+
+from itertools import combinations
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.stattools import acf, pacf
+from statsmodels.tsa.arima_model import ARIMA as ARIMA
+import statsmodels.api as sm
+import statsmodels.tsa.api as smt
+
+def test_stationarity(timeseries):
+    #Determing rolling statistics
+    MA = timeseries.rolling(window = 12).mean()
+    MSTD = timeseries.rolling(window = 12).std()
+
+    #Plot rolling statistics:
+    plt.figure(figsize=(15,5))
+    orig = plt.plot(timeseries, color='blue',label='Original')
+    mean = plt.plot(MA, color='red', label='Rolling Mean')
+    std = plt.plot(MSTD, color='black', label = 'Rolling Std')
+    plt.legend(loc='best')
+    plt.title('Rolling Mean & Standard Deviation')
+    plt.show(block=False)
+
+    #Perform Dickey-Fuller test:
+    print('Results of Dickey-Fuller Test:')
+    dftest = adfuller(timeseries, autolag='AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key] = value
+    print(dfoutput)
+
+def tsplot(y, lags=None, name = '', figsize=(12, 7), style='bmh'):
+    if not isinstance(y, pd.Series):
+        y = pd.Series(y)
+        
+    with plt.style.context(style):    
+        fig = plt.figure(figsize=figsize)
+        layout = (2, 2)
+        ts_ax = plt.subplot2grid(layout, (0, 0), colspan=2)
+        acf_ax = plt.subplot2grid(layout, (1, 0))
+        pacf_ax = plt.subplot2grid(layout, (1, 1))
+        
+        y.plot(ax=ts_ax)
+        p_value = sm.tsa.stattools.adfuller(y)[1]
+        ts_ax.set_title('Time Series Analysis Plots\n Dickey-Fuller: p={0:.5f}'.format(p_value))
+        smt.graphics.plot_acf(y, lags=lags, ax=acf_ax)
+        smt.graphics.plot_pacf(y, lags=lags, ax=pacf_ax)
+        plt.tight_layout()
+        plt.savefig(name + '_pacf.jpg')
+        fig.show()
